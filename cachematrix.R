2ac1f4f215b56859a15ccb1327b422bef1efad9c
theperic <theperic@gmail.com>
2014-06-22 06:30:54 -0700

    Revert "I modified the stub code to invert a matrix instead of calculating a mean"
    This reverts commit ee9ad62bc4b178bf28b3a2aacd0311f29586406f.

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,15 @@
-## To save processing time this script creates an object that stores the cache of the inverse of a matrix.
-## There are two functions: 
-## 1) makeCacheMatrixfunctions to create a object with the matrix and its inverse and 
-## 2) cacheSolve to efficiently retreive or recalculate the inverse as changes occur to the matrix.
+## Put comments here that give an overall description of what your
+## functions do
 
-
-## makeCacheMatrix creates the oject with teh matrix and the inverse for future use
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }cache
-  get <- function() x
-  setinv <- function(solve) m <<- solve
-  getinv <- function() m
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
-  
-  
+
 }
 
 
-## This funciton first checks if the inverse has been set.  if so it returns it.
-##  if not, it calculates it, caches it, then returns it.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinv()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
-  m
-  
 }
